29500692bd2bc75e0c292e7ea1615d6f99aab8a9
SantiSosa02 <santi4586sosa@gmail.com>
2023-11-21 17:26:44 -0500

    correccion

--- src/routes/usuarioRoutes.js
+++ src/routes/usuarioRoutes.js
@@ -4,19 +4,19 @@ const UserController = require ('../controllers/usuarioController');
 const verificarToken = require('../middleware/verificar-token');
 
 // Ruta para obtener todos los usuarios
-router.get('/usuarios',verificarToken, UserController.getAllUsers);
-router.get('/usuarios/:id',verificarToken, UserController.getUserById);
-router.post('/usuarios', verificarToken,UserController.createUser);
-router.put('/usuarios/:id',verificarToken, UserController.updateUser);
-router.put('/usuarios/estado/:id',verificarToken, UserController.updateUserState);
-router.delete('/usuarios/:id',verificarToken, UserController.deleteUser)
+router.get('/usuarios', UserController.getAllUsers);
+router.get('/usuarios/:id', UserController.getUserById);
+router.post('/usuarios',UserController.createUser);
+router.put('/usuarios/:id', UserController.updateUser);
+router.put('/usuarios/estado/:id', UserController.updateUserState);
+router.delete('/usuarios/:id', UserController.deleteUser)
 router.post('/usuarios/login' , UserController.loginUser);
 router.post('/usuarios/recuperar', UserController.forgotPassword);
 router.post('/cambiar-contrasena/:token', UserController.changePassword);
-router.get('/usuarios-activos',verificarToken, UserController.getActiveUsers);
-router.get('/usuarios-inactivos',verificarToken, UserController.getInactiveUsers);
-router.get('/usuarios-buscar',verificarToken, UserController.searchUser);
-router.get('/usuarios-correo',verificarToken, UserController.verificarCorreoExistente);
+router.get('/usuarios-activos', UserController.getActiveUsers);
+router.get('/usuarios-inactivos', UserController.getInactiveUsers);
+router.get('/usuarios-buscar', UserController.searchUser);
+router.get('/usuarios-correo', UserController.verificarCorreoExistente);
 
 
 module.exports = router;
