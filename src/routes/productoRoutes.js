30890d93aeb12a598cce84f10f730ad410cd3573
SantiSosa02 <santi4586sosa@gmail.com>
2023-11-21 20:00:38 -0500

    Token productos

--- src/routes/productoRoutes.js
+++ src/routes/productoRoutes.js
@@ -5,19 +5,19 @@ const verificarToken = require('../middleware/verificar-token');
 
 
 // Ruta para obtener todos los usuarios
-router.get('/productos',ProductController.getAllProducts);
-router.get('/productos/:id', ProductController.getProductById);
-router.post('/productos', ProductController.createProduct);
-router.put('/productos/:id', ProductController.updateProduct);
-router.delete('/productos/:id', ProductController.deleteProduct)
-router.put('/productos/estado/:id', ProductController.updateProductState);
-router.get('/productos-activos', ProductController.getActiveProducts);
-router.get('/productos-inactivos',ProductController.getInactiveProducts);
-router.get('/productos-buscar', ProductController.searchProduct);
-router.get('/productos-nombre', ProductController.verificarNombreExistente);
-router.put('/productos-agregar/:id', ProductController.agregarCantidad);
-router.get('/productos-cantidad/:id',ProductController.getCantidadActual);
-router.get('/productos-categoria/:idcategoria',ProductController.getProductosByCategoria);
+router.get('/productos',verificarToken,ProductController.getAllProducts);
+router.get('/productos/:id',verificarToken, ProductController.getProductById);
+router.post('/productos', verificarToken,ProductController.createProduct);
+router.put('/productos/:id', verificarToken,ProductController.updateProduct);
+router.delete('/productos/:id', verificarToken,ProductController.deleteProduct)
+router.put('/productos/estado/:id', verificarToken,ProductController.updateProductState);
+router.get('/productos-activos',verificarToken, ProductController.getActiveProducts);
+router.get('/productos-inactivos',verificarToken,ProductController.getInactiveProducts);
+router.get('/productos-buscar',verificarToken, ProductController.searchProduct);
+router.get('/productos-nombre', verificarToken,ProductController.verificarNombreExistente);
+router.put('/productos-agregar/:id', verificarToken,ProductController.agregarCantidad);
+router.get('/productos-cantidad/:id',verificarToken,ProductController.getCantidadActual);
+router.get('/productos-categoria/:idcategoria',verificarToken,ProductController.getProductosByCategoria);
 
 
 module.exports = router;
