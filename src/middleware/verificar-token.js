4e92a7712fb69753d5f1820f46307fffcc79934c
SantiSosa02 <santi4586sosa@gmail.com>
2024-02-19 12:59:39 -0500

    oe

--- src/middleware/verificar-token.js
+++ src/middleware/verificar-token.js
@@ -1,21 +1,25 @@
 const jwt = require('jsonwebtoken');
-require ('dotenv').config();
+require('dotenv').config();
 
 function verificarToken(req, res, next) {
-    const token= req.header('x-token');
+    const token = req.header('x-token');
 
-    if(!token){
-        return res.status(401).json({error: "Acceso no autorizado. Token no proporcionado."})
+    if (!token) {
+        return res.status(401).json({ error: "Acceso no autorizado. Token no proporcionado." });
     }
 
-    try{
+    try {
         const decoded = jwt.verify(token, process.env.SECRETORPRIVATEKEY);
         console.log(decoded);
         req.usuario = decoded.usuario;
         next();
-    }catch (error){
+    } catch (error) {
         console.log(error);
-        return res.status(401).json({error: 'Token no valiodo.'});
+        if (error.name === 'TokenExpiredError') {
+            return res.status(403).json({ error: 'Token expirado.' });
+        } else {
+            return res.status(401).json({ error: 'Token no válido.' });
+        }
     }
 }
 
