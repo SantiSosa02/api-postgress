cba9266047a3c01aab2355980b0296d090fe77fb
SantiSosa02 <santi4586sosa@gmail.com>
2024-02-19 12:40:18 -0500

    regresar cambios

--- src/helpers/generar-jwt.js
+++ src/helpers/generar-jwt.js
@@ -1,32 +1,25 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-const REFRESH_WINDOW = 60; // 60 segundos, 1 minuto
-
 const generarJWT = (id = '') => {
-    return new Promise((resolve, reject) => {
-        const payload = { id };
 
-        jwt.sign(payload, process.env.SECRETORPRIVATEKEY, {
-            expiresIn: '2m' // El token expira en 2 minutos
-        }, (err, token) => {
-            if (err) {
+    return new Promise( (resolve, reject) => {
+
+        const payload = { id }
+
+        jwt.sign(payload , process.env.SECRETORPRIVATEKEY ,{
+            expiresIn:'1m'
+        }, (err, token)=>{
+            if(err){
                 console.log(err);
-                reject('No se pudo generar el token');
-            } else {
-                resolve(token);
+                reject('No se puedo generar el toekn')
+            }else{
+                resolve(token)
             }
-        });
-    });
-};
+        })
+    })
+}
 
-const refrescarJWT = (id = '') => {
-    console.log("Token refrescado"); // Mensaje de registro para comprobar que el token se está refrescando
-    return generarJWT(id);
-};
-
-module.exports = {
-    generarJWT,
-    refrescarJWT,
-    REFRESH_WINDOW
-};
+module.exports= {
+    generarJWT
+}
