6d4da07fd9fce08cd1ed65bb57e12ec83a728328
SantiSosa02 <santi4586sosa@gmail.com>
2024-03-20 20:36:32 -0500

    10 segundos de token

--- src/helpers/generar-jwt.js
+++ src/helpers/generar-jwt.js
@@ -8,7 +8,7 @@ const generarJWT = (id = '') => {
         const payload = { id }
 
         jwt.sign(payload , process.env.SECRETORPRIVATEKEY ,{
-            expiresIn:'8h'
+            expiresIn:'10s'
         }, (err, token)=>{
             if(err){
                 console.log(err);
